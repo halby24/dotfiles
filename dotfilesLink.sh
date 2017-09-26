3d344fe4f964e3157e7c5940ed322de64ea7c58b
halby <halby24@gmail.com>
2017-09-27 00:30:02 +0900

    変数名変えた

--- dotfilesLink.sh
+++ dotfilesLink.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 SCRIPT_DIR=$(cd $(dirname $0); pwd)
-ln -sf $SCRIPT_DIR/.vimrc ~/.vimrc
-ln -sf $SCRIPT_DIR/.vim ~/.vim
-ln -sf $SCRIPT_DIR/.bash_profile ~/.bash_profile
-ln -sf $SCRIPT_DIR/.bashrc ~/.bashrc
-ln -sf $SCRIPT_DIR/.dein.toml ~/.dein.toml
-ln -sf $SCRIPT_DIR/.dein_lazy.toml ~/.dein_lazy.toml
+ln -sf ${SCRIPT_DIR}/.vimrc ~/.vimrc
+ln -sf ${SCRIPT_DIR}/.vim ~/.vim
+ln -sf ${SCRIPT_DIR}/.bash_profile ~/.bash_profile
+ln -sf ${SCRIPT_DIR}/.bashrc ~/.bashrc
+ln -sf ${SCRIPT_DIR}/.dein.toml ~/.dein.toml
+ln -sf ${SCRIPT_DIR}/.dein_lazy.toml ~/.dein_lazy.toml
